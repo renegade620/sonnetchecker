6ec29cbd9b385a627affff7b12c2bea1f58563f8
Frankline Were <franklinegift@gmail.com>
2026-08-01 16:07:32 +0300

    feat: update sonnet types

--- sonnet_checker/sonnet_types.py
+++ sonnet_checker/sonnet_types.py
@@ -1,10 +1,37 @@
 SONNET_TYPES = {
     "shakespearean": {
+        "name": "Shakespearean Sonnet",
         "line_count": 14,
-        "rhyme_scheme": "ABAB CDCD EFEF GG",
+        "rhyme_scheme": [
+            "A", "B", "A", "B",
+            "C", "D", "C", "D",
+            "E", "F", "E", "F",
+            "G", "G"
+        ],
+        "meter": "iambic pentameter",
     },
+
     "petrarchan": {
+        "name": "Petrarchan Sonnet",
         "line_count": 14,
-        "rhyme_scheme": "ABBA ABBA CDE CDE",
+        "rhyme_scheme": [
+            "A", "B", "B", "A",
+            "A", "B", "B", "A",
+            "C", "D", "E",
+            "C", "D", "E"
+        ],
+        "meter": "iambic pentameter",
     },
+
+    "spenserian": {
+        "name": "Spenserian Sonnet",
+        "line_count": 14,
+        "rhyme_scheme": [
+            "A", "B", "A", "B",
+            "B", "C", "B", "C",
+            "C", "D", "C", "D",
+            "E", "E"
+        ],
+        "meter": "iambic pentameter",
+    }
 }
