2e1759dee53feec86f5b308ea73e0aa5944f3496
Frankline Were <franklinegift@gmail.com>
2026-08-01 15:54:45 +0300

    feat: add sonnet types

---
+++ sonnet_checker/sonnet_types.py
@@ -0,0 +1,10 @@
+SONNET_TYPES = {
+    "shakespearean": {
+        "line_count": 14,
+        "rhyme_scheme": "ABAB CDCD EFEF GG",
+    },
+    "petrarchan": {
+        "line_count": 14,
+        "rhyme_scheme": "ABBA ABBA CDE CDE",
+    },
+}
